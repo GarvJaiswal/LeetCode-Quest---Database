a7a558fb61b4e2784bd71bf41d13619fa1392a05
GarvJaiswal <garvjaiswal10@gmail.com>
2026-08-04 13:22:05 +0530

    Solutiong to tree node

---
+++ GroupingAndAggregation/Q3-Tree-Node.sql
@@ -0,0 +1,13 @@
+-- -- Write your PostgreSQL query statement below
+
+SELECT
+    t1.id,
+    CASE
+        WHEN t1.p_id IS NULL THEN 'Root'
+        WHEN COUNT(t2.id) = 0 THEN 'Leaf'
+        ELSE 'Inner'
+    END AS type
+FROM Tree t1
+LEFT JOIN Tree t2
+ON t1.id = t2.p_id
+GROUP BY t1.id, t1.p_id;
